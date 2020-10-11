0c765d7a938cf4eb6c0ded73cc5efabf171fa1e5
HJ94 <50905731+HJ94@users.noreply.github.com>
2020-10-11 18:15:53 +0900

    입금,출금,회원 탈퇴 기능 구현

--- MainLayout.java
+++ MainLayout.java
@@ -28,19 +28,20 @@ public class MainLayout extends Frame implements ActionListener {
 	// List<Member> list = new Vector<>();
 	Panel[] p = new Panel[10];
 	Label[] l = new Label[100];
-	public JButton[] b = new JButton[20];
-	public TextField[] tf = new TextField[10];
-	public Dialog dialog1, dialog2;
-	public Image img;
+	JButton[] b = new JButton[20];
+	TextField[] tf = new TextField[10];
+	Dialog dialog1, dialog2;
+	Image img;
 	String UserName;
 	int UserBalance, deposit, withdraw;
 	String driver = "com.mysql.cj.jdbc.Driver";
-	String url = "jdbc:mysql://localhost:3306/app?characterEnconding=UTF-8&serverTimezone=UTC"; //url 정보
+	String url = "jdbc:mysql://localhost:3306/app?characterEnconding=UTF-8&serverTimezone=UTC"; // url 정보
 	Connection conn = null;
 	Statement stmt = null;
 	ResultSet rs = null;
-	
+
 	public MainLayout() {
+		
 		String id = Bank_Layout.tf[0].getText();
 		String pw = Bank_Layout.tf[1].getText();
 		try {
@@ -50,7 +51,7 @@ public class MainLayout extends Frame implements ActionListener {
 			String sql = "select * from member";
 			rs = stmt.executeQuery(sql);
 			while(rs.next()) {
-				if(id.equals(rs.getString("ID"))) {
+				if(id.equals(rs.getString("ID")) && pw.equals(rs.getString("PW"))) {
 					UserName = rs.getString("NAME");
 					//UserBalance = rs.getInt("BALANCE");
 				}
@@ -189,28 +190,78 @@ public class MainLayout extends Frame implements ActionListener {
 		b[10].addActionListener(this);
 		b[10].addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Deposit();
-				
-			}
-			
+				Deposit();	
+				b[16].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String id = Bank_Layout.tf[0].getText();
+						String pw = Bank_Layout.tf[0].getText();
+
+						try {
+							Class.forName(driver);
+							conn = DriverManager.getConnection(url, "root", "java");
+							stmt = conn.createStatement();
+							String sql = "select * from member";
+							rs = stmt.executeQuery(sql);
+							while(rs.next()) {
+								if(id.equals(rs.getString("ID")) && pw.equals(rs.getString("PW"))) {
+									UserBalance = rs.getInt("BALANCE");
+								}
+							}
+							UserBalance += Integer.parseInt(tf[5].getText());
+							String sql2 = "update member set BALANCE = '" + UserBalance + "'WHERE ID = '" + UserName + "'" ;
+							int result = stmt.executeUpdate(sql2);
+							String msg = result > -1 ? "successful" : "fail";
+							System.out.println(msg);
+							}catch(Exception ex) {
+							System.out.println("데이터 찾기 실패");
+						}finally {
+							try {
+								if(rs != null)rs.close();
+								if(stmt != null)stmt.close();
+								if(conn != null)conn.close();
+							}catch(Exception ex) {}
+						}
+							}
+						});
+			}	
 		});
-		// <출금>
-		b[16].addActionListener(this);
-		b[16].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-						
-					}
-				});
-		
-		
-		
-		
-		
-//
 //		// <출금>
 		b[11].addActionListener(this);
 		b[11].addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Withdraw();
+				b[17].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String id = Bank_Layout.tf[0].getText();
+						String pw = Bank_Layout.tf[0].getText();
+
+						try {
+							Class.forName(driver);
+							conn = DriverManager.getConnection(url, "root", "java");
+							stmt = conn.createStatement();
+							String sql = "select * from member";
+							rs = stmt.executeQuery(sql);
+							while(rs.next()) {
+								if(id.equals(rs.getString("ID")) && pw.equals(rs.getString("PW"))) {
+									UserBalance = rs.getInt("BALANCE");
+								}
+							}
+							UserBalance -= Integer.parseInt(tf[6].getText());
+							String sql2 = "update member set BALANCE = '" + UserBalance + "'WHERE ID = '" + UserName + "'" ;
+							int result = stmt.executeUpdate(sql2);
+							String msg = result > -1 ? "successful" : "fail";
+							System.out.println(msg);
+							}catch(Exception ex) {
+							System.out.println("데이터 찾기 실패");
+						}finally {
+							try {
+								if(rs != null)rs.close();
+								if(stmt != null)stmt.close();
+								if(conn != null)conn.close();
+							}catch(Exception ex) {}
+						}
+							}
+						});
 				
 			}
 		});
@@ -219,6 +270,27 @@ public class MainLayout extends Frame implements ActionListener {
 		b[13].addActionListener(this);
 		b[13].addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName(driver);
+					conn = DriverManager.getConnection(url, "root", "java");
+					stmt = conn.createStatement();
+					String sql = "select * from member";
+					rs = stmt.executeQuery(sql);
+					while(rs.next()) {
+						if(id.equals(rs.getString("ID")) && pw.equals(rs.getString("PW"))) {
+							UserBalance = rs.getInt("BALANCE");
+						}
+					}
+					l[54].setText(Integer.toString(UserBalance) + "원");
+					}catch(Exception ex) {
+					System.out.println("데이터 찾기 실패");
+				}finally {
+					try {
+						if(rs != null)rs.close();
+						if(stmt != null)stmt.close();
+						if(conn != null)conn.close();
+					}catch(Exception ex) {}
+				}
 				l[54].setText(Integer.toString(UserBalance) + "원");
 			}
 		});
@@ -230,6 +302,15 @@ public class MainLayout extends Frame implements ActionListener {
 				setVisible(false);
 			}
 		});
+		//<개인 정보 변경> - Update
+		b[12].addActionListener(this);
+		b[12].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ChangeInfo();
+			}	
+		});	
+		
+		
 //		
 //		//<회원 탈퇴>
 //		b[6].addActionListener(this);
@@ -258,103 +339,233 @@ public class MainLayout extends Frame implements ActionListener {
 			}
 		});
 		//<회원 정보 삭제>
-//		b[7].addActionListener(this);
-//		b[7].addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				try {
-//					Class.forName(driver);
-//					conn = DriverManager.getConnection(url, "root", "java");
-//					stmt = conn.createStatement();
-//					String sql = "DELETE FROM member WHERE ID = '" + tf[3] +"' AND PW = '" + tf[4] + "'";
-//					int result = stmt.executeUpdate(sql);
-//					String msg = result >  -1 ? "successful" : "fail";
-//					System.out.println(msg);
-//				}catch(Exception ex) {
-//					ex.printStackTrace();
-//				}finally {
-//					try {
-//						if (stmt != null)stmt.close();
-//						if (conn != null)conn.close();
-//					}catch(Exception ex) {	
-//					}
-//				}	
-//			}
-//		});
-		
-		
-		
-	}
+		b[15].addActionListener(this);
+		b[15].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DeleteUser();
+				b[7].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					String id = tf[3].getText();
+					String pw = tf[4].getText();	
+					try
+					{
+						Class.forName(driver);
+						conn = DriverManager.getConnection(url, "root", "java");
+						stmt = conn.createStatement();
+						String sql = "DELETE FROM member WHERE ID = '" + id + "' AND PW = '" + pw + "'";
+						int result = stmt.executeUpdate(sql);
+						String msg = result > -1 ? "successful" : "fail";
+						System.out.println(msg);
+					}catch(
+					Exception ex)
+					{
+						ex.printStackTrace();
+					}finally
+					{
+						try {
+							if (stmt != null)
+								stmt.close();
+							if (conn != null)
+								conn.close();
+						} catch (Exception ex) {
+						}
+					}
+					
+					}
+				});			
+			}
+		});	
+	}// MainLayout () end
+
+	
+
 	public void Deposit() {
 		Frame fr2 = new Frame();
 		p[6] = new Panel();
-		p[6].setLayout(new GridLayout(3,3));
-		
+		p[6].setLayout(new GridLayout(3, 3));
+
 		l[59] = new Label("");
 		l[60] = new Label("입금하기");
 		l[61] = new Label("");
-		
+
 		l[62] = new Label("금액 입력");
 		tf[5] = new TextField();
 		b[16] = new JButton("입금");
-				
+
 		l[65] = new Label("");
 		l[66] = new Label("");
 		l[67] = new Label("");
-		
+
 		p[6].add(l[59]);
 		p[6].add(l[60]);
 		p[6].add(l[61]);
-		
+
 		p[6].add(l[62]);
 		p[6].add(tf[5]);
 		p[6].add(b[16]);
-	
+
 		p[6].add(l[65]);
 		p[6].add(l[66]);
 		p[6].add(l[67]);
-		
+
 		fr2.add(p[6]);
-		
-		fr2.setSize(350,200);
+
+		fr2.setSize(350, 200);
 		fr2.setVisible(true);
+	}
+
+	public void Withdraw() {
+		Frame fr3 = new Frame();
+		p[7] = new Panel();
+		p[7].setLayout(new GridLayout(3, 3));
+
+		l[68] = new Label("");
+		l[69] = new Label("출금하기");
+		l[70] = new Label("");
+
+		l[71] = new Label("금액 입력");
+		tf[6] = new TextField();
+		b[17] = new JButton("출금");
+
+		l[72] = new Label("");
+		l[73] = new Label("");
+		l[74] = new Label("");
+
+		p[7].add(l[68]);
+		p[7].add(l[69]);
+		p[7].add(l[70]);
+
+		p[7].add(l[71]);
+		p[7].add(tf[6]);
+		p[7].add(b[17]);
+
+		p[7].add(l[72]);
+		p[7].add(l[73]);
+		p[7].add(l[74]);
+
+		fr3.add(p[7]);
+		fr3.setSize(350, 200);
+		fr3.setVisible(true);
+	}
+	//회원 정보 변경
+	public void ChangeInfo() {
+		Frame fr4 = new Frame();
+		p[8] = new Panel();
+		fr4.setLayout(new BorderLayout());
+		p[8].setLayout(new GridLayout(1,3));
 		
+		l[75] = new Label("회원 정보 변경");
+		l[75].setFont(new Font("Bodeni MT Black", Font.BOLD, 20));
+		l[76] = new Label("");
+		l[77] = new Label("");
+		
+		p[8].add(l[75]);
+		p[8].add(l[76]);
+		p[8].add(l[77]);
+		fr4.add(p[8], "North");
+		
+		p[9] = new Panel();
+		p[9].setLayout(new GridLayout(5,4));
+		
+		l[78] = new Label("");
+		l[79] = new Label("");
+		l[80] = new Label("");
+		l[81] = new Label("");
+		
+		l[82] = new Label("");
+		l[83] = new Label("ID : ");
+		tf[7] = new TextField();
+		l[84] = new Label("");
+		
+		l[85] = new Label("");
+		l[86] = new Label("");
+		tf[8] = new TextField();
+		l[87] = new Label("");
+		
+		l[88] = new Label("");
+		l[89] = new Label("");
+		l[90] = new Label("");
+		l[91] = new Label("");
+		
+		l[92] = new Label("");
+		b[9] = new JButton("확인");
+		b[10] = new JButton("확인");
+		l[93] = new Label("");
+		
+		p[9].add(l[78]);
+		p[9].add(l[79]);
+		p[9].add(l[80]);
+		p[9].add(l[81]);
+		
+		p[9].add(l[82]);
+		p[9].add(l[83]);
+		p[9].add(tf[7]);
+		p[9].add(l[84]);
+		
+		p[9].add(l[85]);
+		p[9].add(l[86]);
+		p[9].add(tf[8]);
+		p[9].add(l[87]);
+		
+		p[9].add(l[88]);
+		p[9].add(l[89]);
+		p[9].add(l[90]);
+		p[9].add(l[91]);
+		
+		p[9].add(l[92]);
+		p[9].add(b[9]);
+		p[9].add(b[10]);
+		p[9].add(l[93]);
+		
+		l[94] = new Label("");
+		l[95] = new Label("");
+		l[96] = new Label("");
+		
+		fr4.add(p[9], "Center");
+		fr4.add(l[94], "East");
+		fr4.add(l[95], "West");
+		fr4.add(l[96], "South");
+		
+		fr4.setSize(300,300);
+		fr4.setVisible(true);
 		
 	}
-	//회원 정보 삭제 메서드
+
+	// 회원 정보 삭제 메서드
 	public void DeleteUser() {
 		Frame fr = new Frame();
-		p[4] = new Panel(); 
+		p[4] = new Panel();
 		fr.setLayout(new BorderLayout());
-		p[4].setLayout(new GridLayout(1,3));
+		p[4].setLayout(new GridLayout(1, 3));
 
 		l[14] = new Label("회원 탈퇴");
 		l[14].setFont(new Font("Bodeni MT Black", Font.BOLD, 20));
 		l[15] = new Label("");
 		l[16] = new Label("");
-		
+
 		p[4].add(l[15]);
 		p[4].add(l[14]);
 		p[4].add(l[16]);
-		fr.add(p[4],"North");
-		
+		fr.add(p[4], "North");
+
 		p[5] = new Panel();
-		p[5].setLayout(new GridLayout(5,4));
-		
+		p[5].setLayout(new GridLayout(5, 4));
+
 		l[17] = new Label("");
 		l[18] = new Label("");
 		l[19] = new Label("");
 		l[20] = new Label("");
-		
+
 		l[21] = new Label("");
 		l[22] = new Label("ID : ");
 		tf[3] = new TextField();
 		l[23] = new Label("");
-		
+
 		l[24] = new Label("");
 		l[25] = new Label("PW : ");
 		tf[4] = new TextField();
 		l[26] = new Label("");
-		
+
 		l[27] = new Label("");
 		l[28] = new Label("");
 		l[29] = new Label("");
@@ -364,33 +575,31 @@ public class MainLayout extends Frame implements ActionListener {
 		b[7] = new JButton("확인");
 		b[8] = new JButton("취소");
 		l[32] = new Label("");
-		
-		
+
 		p[5].add(l[17]);
 		p[5].add(l[18]);
 		p[5].add(l[19]);
 		p[5].add(l[20]);
-		
+
 		p[5].add(l[21]);
 		p[5].add(l[22]);
 		p[5].add(tf[3]);
 		p[5].add(l[23]);
-		
+
 		p[5].add(l[24]);
 		p[5].add(l[25]);
 		p[5].add(tf[4]);
 		p[5].add(l[26]);
-		
+
 		p[5].add(l[27]);
 		p[5].add(l[28]);
 		p[5].add(l[29]);
 		p[5].add(l[30]);
-		
+
 		p[5].add(l[31]);
 		p[5].add(b[7]);
 		p[5].add(b[8]);
 		p[5].add(l[32]);
-
 
 		l[33] = new Label("");
 		l[34] = new Label("");
@@ -401,22 +610,18 @@ public class MainLayout extends Frame implements ActionListener {
 		fr.add(l[34], "West");
 		fr.add(l[35], "South");
 
-	
-		fr.setSize(300,300);
+		fr.setSize(300, 300);
 		fr.setVisible(true);
 	}
-	
-	
 
 	class ImagePanel extends Panel {
 		@Override
 		public void paint(Graphics g) {
 			// g.fillRect(x,100,200,200);
 			g.drawImage(img, 0, 0, this);
-		
+
 		}
 	}
-	
 
 	public void actionPerformed(ActionEvent e) {
 		addWindowListener(new WindowAdapter() {
@@ -424,7 +629,6 @@ public class MainLayout extends Frame implements ActionListener {
 			}
 		});
 	}
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
